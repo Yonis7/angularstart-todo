c46fdae9da3f51c93beeb0e162b949d8bf7e06ed
Yonis7 <yonisaden7@gmail.com>
2024-12-11 17:19:00 -0500

    docs: add comment to Todo interface explaining properties

--- src/app/shared/interfaces/todo.ts
+++ src/app/shared/interfaces/todo.ts
@@ -1,3 +1,4 @@
+//This is the interface for the todo object. It has two properties, title and description, both of which are strings. This interface will be used to define the structure of the todo object. The todo object will be used to store the title and description of a todo item.
 export interface Todo {
   title: string;
   description: string;
