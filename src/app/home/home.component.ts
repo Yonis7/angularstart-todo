dbfeb3d8627def496952a47da6ffba560f70346c
Yonis7 <yonisaden7@gmail.com>
2024-12-12 10:57:50 -0500

    feat: add createTodo method to HomeComponent for handling new todos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoFormComponent } from './ui/todo-form.component';
+import { Todo } from '../shared/interfaces/todo';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,9 @@ import { TodoFormComponent } from './ui/todo-form.component';
   standalone: true,
   imports: [TodoFormComponent],
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  // The purpose of this method is to create a new todo. They syntax explained: 'createTodo' is the name of the method, 'todo' is the parameter, and 'Todo' is the type of the parameter. 
+  createTodo = (todo: Todo) => {
+    console.log(todo);
+  }
+}
